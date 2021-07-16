71b5c54c5c207b99c0bdb94fe555ff4b5e5ff647
Benja <otienojaoko@gmail.com>
2021-07-16 21:23:21 +0300

    Add files via upload

---
+++ lab01/arrays.py
@@ -0,0 +1,15 @@
+cars = ["BMW","bugatti","Volvo","Merc"]
+cars.remove("Volvo")
+cars.pop(2)
+cars.append("toyota")
+#cars.clear()
+print(cars)
+x=cars.count("toyota")
+print(cars)
+
+y = len(cars)
+print(y)
+
+cars.sort(reverse=True)
+cars.sort()
+print(cars)
