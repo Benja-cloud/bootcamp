42671bfff2c5ae88ad1bd94fdd4ec521c16dc655
Benja <otienojaoko@gmail.com>
2021-07-19 14:25:25 +0300

    update

---
+++ assignments.py/func.py
@@ -0,0 +1,23 @@
+#functions/variable
+def cars():
+    print("I love cars. They are great!!")
+    
+
+cars()
+#functions with arguments
+def cars(first_car, second_car):
+    print(f"My whole fleet includes a {first_car} and a {second_car}.")
+
+
+cars("BMW", "Merc")
+
+#function with a condition
+
+def cars():
+    if i:
+        print("German machine")
+    else:
+        print("Jap jalopies")
+
+
+cars("BMW","Merc")
