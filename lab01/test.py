9004386f0edc1f58a72e2b695049bef5a9e39741
Benja <otienojaoko@gmail.com>
2021-07-19 15:38:46 +0300

    update

--- lab01/test.py
+++ lab01/test.py
@@ -1,2 +1,20 @@
+#python code to demo
+#instantiation of a class
 class Dog:
     pass
+    #a simple class
+    attr1 = "mammal"
+    attr2 = "dog"
+
+    #a sample method
+    def fun(self):
+        print("I'm a" ,self.attr1)
+        print("I'm a",self.attr2)
+
+#driver Code 
+#object instantiation
+Rodger = Dog()
+#Accessing class attributes
+#and method through objects
+print(Rodger.attr1)
+Rodger.fun()
