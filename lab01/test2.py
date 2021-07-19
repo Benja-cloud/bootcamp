97b1b36b8b0d9e36ba1ff7a5a31b2b20f544edf1
Benja <otienojaoko@gmail.com>
2021-07-19 16:06:03 +0300

    update

---
+++ lab01/test2.py
@@ -0,0 +1,12 @@
+class Person:
+    pass
+    #a simple class
+    def _init_(self, name):
+        self,name = name
+
+    #a sample method
+    def say_hi(self):
+        print("Hello, my name is ", self.name )
+
+p = Person('Otieno')
+p.say.hi()
