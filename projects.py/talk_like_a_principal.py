2716cf06b6005031b1ac036c5a31d39eb89af6da
Benja <otienojaoko@gmail.com>
2021-07-28 18:55:03 +0300

    update

---
+++ projects.py/talk_like_a_principal.py
@@ -0,0 +1,16 @@
+#this program takes user inputs and forms a story out of it
+user_take1 = input("Enter a day of the week: ")
+user_take2 = input("Enter what happened: ")
+user_take3 = input("It had impact in that: ")
+user_take4 = input("Its a day...: ")
+user_take5 = input("So many people were....: ")
+user_take6 = input("Your Conclusion about it: ")
+
+print("So the whole story is this.") 
+
+print("On " + user_take1) 
+print( user_take2 ) 
+print("It had impact in that " + user_take3 ) 
+print("Its a day " + user_take4) 
+print("So many people were " + user_take5) 
+print( user_take6) 
